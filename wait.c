c8ac57561c1ab88d715ec67e28aafc325b850196
JoelJosephJecob <joeljosephjecob@gmail.com>
2024-09-10 15:22:15 +0530

    Add files via upload

---
+++ wait.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int main()
+{
+pid_t q;
+printf("hello! it is before fork\n\n");
+printf("PID=%d\n",getpid());
+q=fork();
+if(q<0)
+{
+printf("error");
+}
+else
+if(q==0)
+{
+printf("i am child ,having pid %d\n",getpid());
+}
+else
+{
+printf("i am parent\n");
+printf("my child's pid is %d\n",q);
+}
+printf("good bye !\n");
+return 0;
+}
+
